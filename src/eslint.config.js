41aca1321875b2a6af287df9e367fb12600eb384
Aleksey Veselov <vesas95@vk.com>
2025-03-05 15:03:05 +0300

    feat(lint): Добавить stylelint

--- src/eslint.config.js
+++ src/eslint.config.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
