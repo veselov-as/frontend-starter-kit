cf0de52264cc8f7ee7805d92ba4053cd00ecefa3
Aleksey Veselov <vesas95@vk.com>
2025-03-06 16:01:41 +0300

    fix(config): Исправить ошибки eslint / vite конфигов

--- src/eslint.config.js
+++ src/eslint.config.js
@@ -1,7 +1,3 @@
-// TODO решить вопрос с типами пакетов
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-
 import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
